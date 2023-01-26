96e2988d35b73976803827d5f2323fcc0e2113d0
amitsisodiya371 <amitsisodiya9045@gmail.com>
2023-01-26 21:58:25 +0530

    port

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Navbar from './Components/Navbar';
-import About from './Components/About';
-import Projects from './Components/Projects';
-import Skills from './Components/Skills';
-import Resume from './Components/Resume';
-import Home from './Components/Home';
-import Contact from './Components/Contact';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Components/About/About';
+import Projects from './Components/Projects/Projects';
+import Skills from './Components/Skills/Skills';
+import Resume from './Components/Resume/Resume';
+import Home from './Components/Home/Home';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
